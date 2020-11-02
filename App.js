f742d0e6aae3824f62b0004bc6bda29ba54b596a
Long Trần <hoanglong040800@gmail.com>
2020-11-02 22:51:36 +0700

    style focused on BottomTabNavigator

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import 'react-native-gesture-handler'
 import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
-import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons'
+import { AntDesign, MaterialCommunityIcons} from '@expo/vector-icons'
 
 import { HomeScreen } from './src/screens/HomeScreen'
 import { HistoryScreen } from './src/screens/HistoryScreen'
@@ -11,49 +11,76 @@ import { AccountScreen } from './src/screens/AccountScreen'
 import FloatingButton from './src/components/FloatingButton'
 
 
+
 const TabNavigator = createBottomTabNavigator(
 	{
 		Home: {
 			screen: HomeScreen,
 			navigationOptions: {
-				tabBarIcon: () => <AntDesign name='home' size={24} color="#cdccce" />
-			}
+				tabBarIcon: ({ tintColor, focused }) => <AntDesign
+					name='home'
+					size={focused ? 32 : 28}
+					color={tintColor} />,
+			},
 		},
 
 		History: {
 			screen: HistoryScreen,
 			navigationOptions: {
-				tabBarIcon: () => <MaterialCommunityIcons name='history' size={24} color="#cdccce" />
-			}
+				tabBarIcon: ({ tintColor, focused }) => <MaterialCommunityIcons
+					name='history'
+					size={focused ? 32 : 28}
+					color={tintColor} />
+			},
 		},
 
 		FltButton: {
 			screen: () => null,
 			navigationOptions: {
-				tabBarIcon: <FloatingButton />
-			}
+				tabBarIcon: <FloatingButton />,
+				tabBarLabel: () => null,
+			},
 		},
 
 		Card: {
 			screen: CardScreen,
 			navigationOptions: {
-				tabBarIcon: () => <AntDesign name='creditcard' size={22} color='#cdccce' />
-			}
+				tabBarIcon: ({ tintColor, focused }) => <AntDesign
+					name='creditcard'
+					size={focused ? 28 : 24}
+					color={tintColor} />
+			},
 		},
 
 		Account: {
 			screen: AccountScreen,
 			navigationOptions: {
-				tabBarIcon: () => <MaterialCommunityIcons name='account' size={24} color='#cdccce' />
+				tabBarIcon: ({ tintColor, focused }) => <MaterialCommunityIcons
+					name='account'
+					size={focused ? 32 : 28}
+					color={tintColor} />
 			}
 		},
 	}, // Button
 
+
 	{
 		tabBarOptions: {
-			showLabel: false
-		}
+			showLabel: true,
+			activeTintColor: '#2CC197',
+			inactiveTintColor: '#CDCCCE',
+			style: {
+				height: 65,
+			},
+			tabStyle: {
+				paddingVertical: 5,	
+			},
+			labelStyle: {
+				fontSize: 11,
+			}
+		},
 	},
+
 )
 
 export default createAppContainer(TabNavigator)
