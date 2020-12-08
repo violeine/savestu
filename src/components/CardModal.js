9b02e5544ee9227ca9e7c6fba8b0f87938b14c8e
ueihvn <letronghieu3007@gmail.com>
2020-12-08 17:33:47 +0700

    update Cardmodal

--- src/components/CardModal.js
+++ src/components/CardModal.js
@@ -1,30 +1,57 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet, 
   TouchableOpacity, 
-  Text, 
-  View, Modal, FlatList,
-  ScrollView, ActivityIndicator} from 'react-native';
+  Text, Button,
+  View, Modal,
+  SectionList} from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
-
+import {useCardDispatch, useCardState} from '../db'
 import {getCard} from "../db/card"
 
-const CardModal = ({visible, hideCardModal, showCardModal, listCards}) => {
-  console.log(listCards)
+const CardModal = ({visible, hideCardModal, showCardModal}) => {
+  const [listCards, setListCards] = useState([]);
+  const dispatch = useCardDispatch();
+  const {id} = useCardState();
+
+  const transferData = (data) => {
+    let result = [
+      {
+        title: 'Using',
+        data: []
+      },
+      {
+        title: 'Saving',
+        data: []
+      }
+    ]
+    data.map((card,index) => {
+      card.type == 'using' ? result[0].data.push(card) : result[1].data.push(card)
+    })
+    return result;
+  }
 
   const renderItem = ({item}) => {
-    <TouchableOpacity
-      onPress={hideCardModal}
-    >
-      <FontAwesome5 name='wallet'/>
-      <View>
-      <Text>{item.type}</Text>
-      <Text>{item.money}</Text>
-      </View>
-    </TouchableOpacity>
+    const backgroundColor = item.id == id ? "#6e3b6e" : "#f9c2ff";
+    return (
+        <TouchableOpacity
+        onPress={() => {
+          dispatch(item);
+          hideCardModal();
+        }}
+        style= {{backgroundColor: backgroundColor}}
+      >
+        <FontAwesome5 name='wallet'/>
+        <View>
+          <Text>{item.type}</Text>
+          <Text>{item.money}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+
   }
 
   return (
-    <View style={styles.centeredView}>
+  <View style={styles.centeredView}>
     <Modal
       animationType="slide"
       transparent={true}
@@ -32,17 +59,23 @@ const CardModal = ({visible, hideCardModal, showCardModal, listCards}) => {
     >
       <View style={styles.centeredView}>
         <View style={styles.modalView}>
-
-        {/* { 
-          listCards ?
-          <FlatList
-            data={listCards._array}
-            renderItem={renderItem}
-            keyExtractor={item => item.id + item.type}
-          />
-          :
-          <ActivityIndicator/> 
-        } */}
+            <Button
+              title='Update'
+              onPress={
+                async () => {
+                  const data = await getCard();
+                  setListCards(data)
+                }
+              }
+            />
+            <SectionList
+              sections={transferData(listCards)}
+              keyExtractor={(item, index) => item+index}
+              renderItem={renderItem}
+              renderSectionHeader={({section: {title}}) => (
+                <Text style={styles.header}>{title}</Text>
+              )}
+            />
         </View>
       </View>
     </Modal>
@@ -51,30 +84,33 @@ const CardModal = ({visible, hideCardModal, showCardModal, listCards}) => {
 };
 
 const styles = StyleSheet.create({
-    centeredView: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    height: 400,
+    width: 300,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
     },
-    modalView: {
-      height: 200,
-      width: 200,
-      flexDirection: 'column',
-      backgroundColor: "white",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5
-    },
-    textStyle: {
-      color: "black",
-      fontWeight: "bold",
-      textAlign: "center"
-    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  textStyle: {
+    color: "black",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  header : {
+    fontSize: 23,
+    marginTop: 5
+  }
 });
 
 export default CardModal;
