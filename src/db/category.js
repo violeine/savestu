229c264fcbd83b340e67451608c5cf83a0efc024
anhvuk13 <anvut20@gmail.com>
2021-01-06 18:21:45 +0700

    categories by card

--- src/db/category.js
+++ src/db/category.js
@@ -11,30 +11,39 @@ export const getCategoryByCard = async (card) => {
   try {
     const [, { rows }] = await execSql(
       `
-      SELECT c.*, SUM(CASE WHEN t.card=? AND t.cash < 0 THEN t.cash ELSE 0 END) sum FROM categories c
+      SELECT c.*, SUM(
+        CASE
+          WHEN t.card=? AND t.cash < 0 THEN -t.cash
+          WHEN t.card=? AND t.cash > 0 THEN t.cash
+          ELSE 0
+        END) sum FROM categories c
       LEFT JOIN transactions t ON c.id = t.category
       GROUP BY c.id, c.name, c.color
+      ORDER BY sum DESC
     `,
-      [card]
+      [card, card]
     );
     return {
       categories: rows._array,
       name: rows._array.map((i) => i.name),
-      color: rows._array.filter(i => i.sum != 0).map((i) => i.color),
-      sum: rows._array.map((i) => i.sum).filter(i => i!= 0),
-      income:rows._array.reduce((acc, el) => {
-        if (el.type =="income") {
-          return acc+el.sum
+      color: rows._array
+        .filter((i) => i.sum != 0 && i.type == "expense")
+        .map((i) => i.color),
+      sum: rows._array
+        .filter((i) => i.sum != 0 && i.type == "expense")
+        .map((i) => i.sum),
+      income: rows._array.reduce((acc, el) => {
+        if (el.type == "income") {
+          return acc + el.sum;
         }
         return acc;
-      },0),
-      expense:rows._array.reduce((acc, el) => {
-        if (el.type =="expense") {
-          return acc+el.sum
+      }, 0),
+      expense: rows._array.reduce((acc, el) => {
+        if (el.type == "expense") {
+          return acc + el.sum;
         }
         return acc;
-      },0)
-
+      }, 0),
     };
   } catch (err) {
     console.log(err);
